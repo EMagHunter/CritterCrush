4ad6978bad50d2c21914884adc7e323be37cbf9f
Ana <anacfg202@gmail.com>
2023-02-14 12:56:20 -0500

    Created app

---
+++ CritterCrush/CritterCrush/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CritterCrush
+//
+//  Created by Ana Fuentes on 2/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
