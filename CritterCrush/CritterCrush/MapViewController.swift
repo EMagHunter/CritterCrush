804813a719d167c21698ea223c6346c45429f86a
Ana <anacfg202@gmail.com>
2023-02-27 12:56:31 -0500

    made new screen, navigationbar, tabbar,

---
+++ CritterCrush/CritterCrush/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  CritterCrush
+//
+//  Created by Ana Fuentes on 2/27/23.
+//
+
+import UIKit
+import MapKit
+import CoreData
+class MapViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView!
+    var managedObjectContext: NSManagedObjectContext!
+}
