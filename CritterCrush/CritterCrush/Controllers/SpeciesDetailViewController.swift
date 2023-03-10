f88304bf7569d1b4e673cc22de9eb8699c5697a4
minjoojeong <minjooj8@gmail.com>
2023-03-10 16:09:47 -0500

    Species view table set up
    set up some variables and features for the tables on species and species detail view, added horizontal scroll image gallery

--- CritterCrush/CritterCrush/Controllers/SpeciesDetailViewController.swift
+++ CritterCrush/CritterCrush/Controllers/SpeciesDetailViewController.swift
@@ -2,21 +2,68 @@
 //  SpeciesDetailViewController.swift
 //  CritterCrush
 //
+//horizontal gallery:
+//https://github.com/zhiyao92/Horizontal-Sliding-Image
+
 //  Created by min joo on 3/9/23.
 //
 
 import UIKit
 
-class SpeciesDetailViewController: UIViewController {
-
-    //programatically based on species
+class SpeciesDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var images: [String] = ["image1","image2","image3"]
+        var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        
+        for index in 0..<images.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imageView = UIImageView(frame: frame)
+            imageView.image = UIImage(named: images[index])
+            self.scrollView.addSubview(imageView)
+        }
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(images.count), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
+    //programatically based on species
+    //protocol stubs for table
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 88
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            return cell
+    }
+
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        var page = scrollView.contentOffset.x/scrollView.frame.size.width
+        pageControl.currentPage = Int(page)
+    }
     
     /*
     // MARK: - Navigation
--- CritterCrush/CritterCrush/Controllers/SpeciesViewController.swift
+++ CritterCrush/CritterCrush/Controllers/SpeciesViewController.swift
@@ -7,18 +7,30 @@
 
 import UIKit
 
-class SpeciesViewController: UIViewController {
-//we gotta rename this
+class SpeciesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var bugName: UILabel!
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    //MARK: table
+    
     //TableView: icons of our bugs
     // 2 columns
     //links to species detail view
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
     
     
     /*
