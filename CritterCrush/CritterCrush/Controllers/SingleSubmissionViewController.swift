df96f3d0dfedb5f89d9533ca19cee6219aeea1dc
minjoojeong <minjooj8@gmail.com>
2023-03-10 14:56:29 -0500

    added more profile screens
    detailed submissions, and individual submissions.
    collection view for detailed

---
+++ CritterCrush/CritterCrush/Controllers/SingleSubmissionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SingleSubmissionViewController.swift
+//  CritterCrush
+//
+//  Created by min joo on 3/10/23.
+//
+
+import UIKit
+
+class SingleSubmissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
