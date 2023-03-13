ee37fa06f024d9f985ad607c520a8d3813227662
minjoojeong <minjooj8@gmail.com>
2023-03-13 02:26:04 -0400

    species table list
    basic code for segue and stuff

--- CritterCrush/CritterCrush/Controllers/SpeciesDetailViewController.swift
+++ CritterCrush/CritterCrush/Controllers/SpeciesDetailViewController.swift
@@ -1,7 +1,7 @@
 //
 //  SpeciesDetailViewController.swift
 //  CritterCrush
-//
+//segue: https://ronm333.medium.com/a-simple-table-view-example-in-swift-cbf9a405f975
 //horizontal gallery:
 //https://github.com/zhiyao92/Horizontal-Sliding-Image
 
@@ -10,60 +10,23 @@
 
 import UIKit
 
-class SpeciesDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+class SpeciesDetailViewController: UIViewController, UITableViewDelegate{
     
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet var speciesName: UILabel!
+    var titleStringViaSegue: String!
+    var bugIDViaSegue: Int!
     
-    var images: [String] = ["image1","image2","image3"]
-        var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        scrollView.showsHorizontalScrollIndicator = false
-        scrollView.isPagingEnabled = true
+        self.speciesName.text = self.titleStringViaSegue
         
-        for index in 0..<images.count {
-            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
-            frame.size = scrollView.frame.size
-            
-            let imageView = UIImageView(frame: frame)
-            imageView.image = UIImage(named: images[index])
-            self.scrollView.addSubview(imageView)
-        }
-        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(images.count), height: scrollView.frame.size.height)
-        scrollView.delegate = self
-        tableView.dataSource = self
-        tableView.delegate = self
+        // Do any additional setup after loading the view.
     }
     
     //programatically based on species
     //protocol stubs for table
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 88
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-            return cell
-    }
-
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        var page = scrollView.contentOffset.x/scrollView.frame.size.width
-        pageControl.currentPage = Int(page)
-    }
     
     /*
     // MARK: - Navigation
