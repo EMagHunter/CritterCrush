120c31ae02b06ea778bfc0daa405b0c494e7b5ca
minjoojeong <minjooj8@gmail.com>
2023-03-19 01:47:31 -0400

    added collection view
    added collection view and corresponding cell

---
+++ CritterCrush/CritterCrush/Controllers/ReportCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReportCell.swift
+//  CritterCrush
+//
+//  Created by min joo on 3/19/23.
+//
+
+import UIKit
+
+class ReportCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
