ad79cebdffd1fbd80497d7fdc61901e50faf5837
Ana <anacfg202@gmail.com>
2023-04-12 20:45:08 -0400

    made a few edits

--- CritterCrush/CritterCrush/Controllers/TabBarViewController.swift
+++ CritterCrush/CritterCrush/Controllers/TabBarViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class TabBarViewController: UITabBarController {
 
-    
+    @IBOutlet weak var tabBAR: UITabBar!
     
     
     override func viewDidLoad() {
+        //tabBAR.backgroundColor = .yellow
         super.viewDidLoad()
+        
        // self.navigationController?.isNavigationBarHidden = true
         // Do any additional setup after loading the view.
     }
