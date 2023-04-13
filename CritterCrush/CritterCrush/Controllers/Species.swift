53114f87475de94003cac5be09aa347db7e3ce39
minjoojeong <minjooj8@gmail.com>
2023-04-12 22:47:42 -0400

    added colors

--- CritterCrush/CritterCrush/Controllers/Species.swift
+++ CritterCrush/CritterCrush/Controllers/Species.swift
@@ -20,13 +20,42 @@ class Species: NSObject {
     var name: String
     var science: String
     var id: Int
+    var color: UIColor
+
+    let salmon = UIColor(red: 0.89, green: 0.62, blue: 0.55, alpha: 1.00)
+        
+    let mustard = UIColor(red: 0.88, green: 0.65, blue: 0.51, alpha: 1.00)
+        
+    let periwinkle = UIColor(red: 0.59, green: 0.72, blue: 0.81, alpha: 1.00)
+        
+    let jade = UIColor(red: 0.55, green: 0.65, blue: 0.64, alpha: 1.00)
     
+    //initialize
     init(name: String, science: String, id: Int){
         self.name = name
         self.science = science
         self.id = id
-    }
+        
+        
+        switch self.id {
+        case 1:
+            self.color = jade
+        case 2:
+            self.color = mustard
+        case 3:
+            self.color = salmon
+        case 4:
+            self.color = periwinkle
+        default:
+            self.color = .black
+        }
+        
+    }//init
+    
+
+    
 }
+
 
 //global variables of Species
 let SLF = Species(name: "Spotted lanternfly", science: "Lycorma delicatula", id: 1)
